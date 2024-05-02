b9fafeddafc362296d242e0c1eecc646749e0762
Shova Yakami <shovayakamionline@gmail.com>
2024-05-02 17:24:02 -0400

    GLAB - 303.11.2 - ArrayList and ArrayList methods

---
+++ src/com/perScholas/glab2ArrayListAndArrayListMethod/CreateArrayListExample.java
@@ -0,0 +1,31 @@
+package com.perScholas.glab2ArrayListAndArrayListMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Example 1: Creating an ArrayList and Adding New Elements
+public class CreateArrayListExample {
+
+    public static void main(String[] args) {
+        // Creating an ArrayList of String
+        List<String> animals = new ArrayList<>();
+
+        // Adding new elements to the ArrayList
+        animals.add("Lion");
+        animals.add("Tiger");
+        animals.add("Cat");
+        animals.add("Dog");
+
+        System.out.println(animals);
+
+        // Adding an element at a particular index in an ArrayList
+        animals.add(2, "Elephant");
+        System.out.println(animals);
+    }
+
+}
+
+/*Output:
+* [Lion, Tiger, Cat, Dog]
+[Lion, Tiger, Elephant, Cat, Dog]
+*/
---
+++ src/com/perScholas/glab2ArrayListAndArrayListMethod/CreateArrayListFromCollectionExample.java
@@ -0,0 +1,73 @@
+package com.perScholas.glab2ArrayListAndArrayListMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Example 2: Creating an ArrayList From Another Collection
+
+public class CreateArrayListFromCollectionExample {
+    public static void main(String[] args) {
+        List<Integer> firstFivePrimeNumbers = new ArrayList<>();
+        firstFivePrimeNumbers.add(2);
+        firstFivePrimeNumbers.add(3);
+        firstFivePrimeNumbers.add(5);
+        firstFivePrimeNumbers.add(7);
+        firstFivePrimeNumbers.add(11);
+
+        // Creating an ArrayList from another collection
+        List<Integer> firstTenPrimeNumbers = new ArrayList<>(firstFivePrimeNumbers);
+
+        List<Integer> nextFivePrimeNumbers = new ArrayList<>();
+        nextFivePrimeNumbers.add(13);
+        nextFivePrimeNumbers.add(17);
+        nextFivePrimeNumbers.add(19);
+        nextFivePrimeNumbers.add(23);
+        nextFivePrimeNumbers.add(29);
+
+        // Adding an entire collection to an ArrayList
+        firstTenPrimeNumbers.addAll(nextFivePrimeNumbers);
+
+        System.out.println(firstTenPrimeNumbers);
+
+        //Example 3: Accessing Elements from an ArrayList
+
+        List<String> topCompanies = new ArrayList<>();
+
+        // Check if an ArrayList is empty
+        System.out.println("Is the topCompanies list empty? : " + topCompanies.isEmpty());
+
+        topCompanies.add("Google");
+        topCompanies.add("Apple");
+        topCompanies.add("Microsoft");
+        topCompanies.add("Amazon");
+        topCompanies.add("Facebook");
+
+        // Find the size of an ArrayList
+        System.out.println("Here are the top " + topCompanies.size() + " companies in the world");
+        System.out.println(topCompanies);
+
+        // Retrieve the element at a given index
+        String bestCompany = topCompanies.get(0);
+        String secondBestCompany = topCompanies.get(1);
+        String lastCompany = topCompanies.get(topCompanies.size() - 1);
+
+        System.out.println("Best Company: " + bestCompany);
+        System.out.println("Second Best Company: " + secondBestCompany);
+        System.out.println("Last Company in the list: " + lastCompany);
+
+        // Modify the element at a given index
+        topCompanies.set(4, "Walmart");
+        System.out.println("Modified top companies list: " + topCompanies);
+    }
+}
+/*Output:
+* [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]*/
+/*Output:
+* Is the topCompanies list empty? : true
+Here are the top 5 companies in the world
+[Google, Apple, Microsoft, Amazon, Facebook]
+Best Company: Google
+Second Best Company: Apple
+Last Company in the list: Facebook
+Modified top companies list: [Google, Apple, Microsoft, Amazon, Walmart]*/
+
