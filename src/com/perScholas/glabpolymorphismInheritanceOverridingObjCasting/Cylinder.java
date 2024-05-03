62a42ce0d8763266d80b15514f1ea24ccbc890fd
Shova Yakami <shovayakamionline@gmail.com>
2024-05-02 20:36:56 -0400

    GLAB - 303.10.2 - Polymorphism, Inheritance, Overriding, Object type Casting.

---
+++ src/com/perScholas/glabpolymorphismInheritanceOverridingObjCasting/Cylinder.java
@@ -0,0 +1,39 @@
+package com.perScholas.glabpolymorphismInheritanceOverridingObjCasting;
+
+//Step - 5
+public class Cylinder extends Circle {
+
+    private final double PI = Math.PI;
+
+    public Cylinder(double radius, double height) {
+        super(radius, height);
+        // TODO Auto-generated constructor stub
+    }
+
+    public Cylinder(double radius) {
+        super(radius);
+    }
+
+    /**
+     * Returns the volume of this cylinder
+     */
+
+    public double getVolume() {
+        return (PI * Math.pow(super.radius, 2)) * super.height;
+    }
+
+    public double getSurfaceArea() {
+        return 2.0 * Math.PI * super.radius * super.height;
+    }
+
+    @Override
+    public void displayShapeName() {
+        System.out.println("Drawing a Cylinder for radius " + super.radius);
+    }
+
+    public String toString() {
+        return "radius is: " + super.radius + "height is : " + super.height;
+
+    }
+}
+
