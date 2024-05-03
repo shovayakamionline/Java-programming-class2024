b73ab909e8dd106f21ef59e44150ebce985ff8dd
Shova Yakami <shovayakamionline@gmail.com>
2024-05-02 20:56:42 -0400

    GLAB - 303.10.3 - Abstraction

---
+++ src/com/perScholas/glabAbstration/Rectangle.java
@@ -0,0 +1,34 @@
+package com.perScholas.glabAbstration;
+
+//Create a class named Rectangle. This will be a Child class. Write the code below:
+public class Rectangle extends Shape {
+    public Rectangle(String color) {
+        super.color = color;
+    }
+
+    public Rectangle() {
+    }
+
+    public Rectangle(String color, double width, double height) {
+        super.height = height;
+        super.width = width;
+        super.color = color;
+    }
+
+    @Override
+    public double getArea() {
+        return super.width * super.height;
+    }
+
+    //Overriding method of base class with different implementation
+    @Override
+    public void displayShapeName() {
+        System.out.println("I am a Rectangle");
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle[height=" + height + ",width=" + width + "," + super.toString() + "]";
+    }
+}
+
---
+++ src/com/perScholas/glabAbstration/Triangle.java
@@ -0,0 +1,38 @@
+package com.perScholas.glabAbstration;
+
+//Create a class named Triangle. This will be a Child class. Write the code below:
+
+public class Triangle extends Shape {
+
+    public Triangle() {
+    }
+
+
+    public Triangle(String color) {
+        super.color = color;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * super.base * super.height;
+    }
+
+    //Overriding method of base class with different implementation
+    @Override
+    public void displayShapeName() {
+        System.out.println("I am a TriAngle");
+    }
+
+    /**
+     * Returns a self-descriptive string
+     */
+    @Override
+    public String toString() {
+        return "Triangle[base=" + super.base + ",height=" + super.height + "," + super.toString() + "]";
+    }
+}
+
