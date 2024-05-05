01a48c4e9454834c1f990d3b974530c909f29bbd
Shova Yakami <shovayakamionline@gmail.com>
2024-05-05 18:39:28 -0400

    Coding Challenge String arithmetic

--- src/com/perScholas/codingchallenge/stringArithmetic/StringArithmetic.java
+++ src/com/perScholas/codingchallenge/stringArithmetic/StringArithmetic.java
@@ -18,62 +18,50 @@ public class StringArithmetic {
     public static void main(String[] args) {
 
         String strNum = "37463738";
-
-//        String[] strArray = new String[strNum.length()/2];
+        int partCount = strNum.length()/4;
         String[] strArray = new String[4];
-//        int j = 0;
 
-        for (int i = 0; i < strNum.length(); i += 2) {
-//          System.out.println(strNum.substring(i, i+2));
-//          strArray[j++] = strNum.substring(i, i+2);
-            strArray[i / 2] = strNum.substring(i, i + 2);
+        for (int i = 0; i < strNum.length(); i += partCount) {
+            strArray[i / partCount] = strNum.substring(i, i + partCount);
         }
-        Arrays.toString(strArray);
+ /*--------------Subtraction-------------------*/
         String str = strArray[0];
-        int difference = str.charAt(0) - str.charAt(1);
+        int firstDigit = str.charAt(0)-'0';
+        int difference = firstDigit;
+        for (int i = 1; i < partCount; i++) {
+            difference -= (str.charAt(i) - '0');
+        }
         System.out.println(difference);
 
+/*--------------divide-------------------*/
         str = strArray[1];
-        float division = (float) (str.charAt(0) - '0') / (str.charAt(1) - '0');
-
+        firstDigit = str.charAt(0) - '0';
+        float division = firstDigit;
+        for (int i = 1; i < partCount; i++) {
+            division = division / (str.charAt(i) - '0');
+        }
         int value = (int) (division * 100);
         division = value / 100f;
         System.out.println(division);
 
+ /*--------------multiplication-------------------*/
         str = strArray[2];
-        int multiply = (str.charAt(0) - '0') * (str.charAt(1) - '0');
+        firstDigit = (str.charAt(0) - '0');
+        int multiply = firstDigit;
+        for (int i = 1; i < partCount; i++) {
+            multiply *= (str.charAt(i) - '0');
+        }
         System.out.println(multiply);
 
+/*--------------Addition-------------------*/
+
         str = strArray[3];
-        int add = (str.charAt(0) - '0') + (str.charAt(1) - '0');
-        System.out.println(add);
-
-        /* Solved with different way*/
-
-        for (int i = 0; i < strArray.length; i++) {
-            int d1 = strArray[i].charAt(0) - '0';
-            int d2 = strArray[i].charAt(1) - '0';
-
-            switch (i) {
-                case 0:
-                    int subtraction = d1 - d2;
-                    System.out.println(subtraction);
-                    break;
-                case 1:
-                    float div = (float) d1 / d2;
-                    int value1 = (int) (div * 100);
-                    div = value1 / 100f;
-                    System.out.println(div);
-                    break;
-                case 2:
-                    int multiplication = d1 * d2;
-                    System.out.println(multiplication);
-                    break;
-                case 3:
-                    int addition = d1 + d2;
-                    System.out.println(addition);
-            }
+        firstDigit = (str.charAt(0) - '0');
+        int add = firstDigit;
+        for (int i = 1; i < partCount ; i++) {
+            add += (str.charAt(i) - '0');
         }
+        System.out.println(add);
     }
 
 }
